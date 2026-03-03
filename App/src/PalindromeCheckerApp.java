4305c98a71fa3eeffe580a07e97fbc1cc561ea2d
AkshitGarg2005 <ag5851@srmist.edu.in>
2026-03-03 09:34:08 +0530

    UC1: Application Entry & Welcome Messag

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+    public static void main(String[] var0) {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version: 1.1");
+        System.out.println("System initialize successfully");
+    }
 }
